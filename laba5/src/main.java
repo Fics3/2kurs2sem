9acfdd53a97e7caecaae29594add833195acc55f
Fics3 <114524392+Fics3@users.noreply.github.com>
2023-03-13 14:35:42 +0300

    Update main.java

--- laba5/src/main.java
+++ laba5/src/main.java
@@ -4,7 +4,7 @@ public class main {
     public static void main(String args[]) {
         IMatrix res = new SquareMatirx( 1000);
         IMatrix test= new SparseMatrix(1000);
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 1000; j++) {
             int row = (int) (Math.random() * (1000-1));
             int column = (int) (Math.random() *  (1000-1));
             int value = (int)(Math.random() * (1000));
